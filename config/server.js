f7c9ef7d955c93fb4fe22d027a9d39104c14fcb7
Francisco Iturriaga <francisco382@gmail.com>
2022-06-06 19:22:45 -0400

    "Add cert conexion"

--- config/server.js
+++ config/server.js
@@ -1,22 +1,27 @@
 const mysql = require("mysql");
+const fs = require("fs");
 
 const db = mysql.createConnection({
-    host:"database-2.cqixht8znhwm.us-east-1.rds.amazonaws.com",
-    port:"3306",
-    user:"admin",
-    password:"helpet-Adm127",
-    database:"helpet",
+    host: "database-2.cqixht8znhwm.us-east-1.rds.amazonaws.com",
+    port: "3306",
+    user: "admin",
+    password: "helpet-Adm127",
+    database: "helpet",
+    ssl:{
+        // cert:'../cert/us-east-1-bundle.pem',
+        ca: fs.readFileSync(__dirname + '../cert/us-east-1-bundle.pem')
+    }
 
 });
-db.connect(err=>{
-    if(err){
+db.connect(err => {
+    if (err) {
         console.log(err.message);
         return;
     }
     db.query("SELECT * FROM user", function (err, result, fields) {
         if (err) throw err;
-       /*  console.log(result); */
-      });
+        /*  console.log(result); */
+    });
     /* console.log("Database connected.") */
 });
 
