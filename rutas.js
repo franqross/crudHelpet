86f9e2cf88f65772af2c385e7c2d08f1817e3be1
Francisco Iturriaga <francisco382@gmail.com>
2022-06-11 17:57:20 -0400

    Add get comunas y regiones

--- rutas.js
+++ rutas.js
@@ -4,12 +4,12 @@ const conexion = require('./config/server');
 //get usuarios
 
 //get all
-router.get('/',(req,res)=>{
-    
+router.get('/usuarios', (req, res) => {
+
     let sql = 'select * from usuario'
-    conexion.query(sql,(err,rows,fields)=>{
-        if(err) throw err;
-        else{
+    conexion.query(sql, (err, rows, fields) => {
+        if (err) throw err;
+        else {
             res.json(rows)
         }
     })
@@ -17,12 +17,12 @@ router.get('/',(req,res)=>{
 
 //por id
 
-router.get('/:id',(req,res)=>{
-    const{id}=req.params
+router.get('/usuario/:id', (req, res) => {
+    const { id } = req.params
     let sql = 'select * from usuario where id_usuario= ?'
-    conexion.query(sql,[id],(err,rows,fields)=>{
-        if(err) throw err;
-        else{
+    conexion.query(sql, [id], (err, rows, fields) => {
+        if (err) throw err;
+        else {
             res.json(rows)
         }
     })
@@ -30,27 +30,27 @@ router.get('/:id',(req,res)=>{
 
 //eliminar
 
-router.delete('/:id',(req,res)=>{
-    const{id}= req.params
-    let sqlfk =`SET FOREIGN_KEY_CHECKS=0;`
-    conexion.query(sqlfk,(err,rows,fields)=>{
-        if(err) throw err;
-        else{
-            
+router.delete('usuario/:id', (req, res) => {
+    const { id } = req.params
+    let sqlfk = `SET FOREIGN_KEY_CHECKS=0;`
+    conexion.query(sqlfk, (err, rows, fields) => {
+        if (err) throw err;
+        else {
+
         }
     })
-    let sql =`delete from usuario where id_usuario='${id}'`
-    conexion.query(sql,(err,rows,fields)=>{
-        if(err) throw err;
-        else{
-            let sqlfk =`SET FOREIGN_KEY_CHECKS=1;`
-            conexion.query(sqlfk,(err,rows,fields)=>{
-                if(err) throw err;
-                else{
-                    res.json({status:'usuario eliminado'})
+    let sql = `delete from usuario where id_usuario='${id}'`
+    conexion.query(sql, (err, rows, fields) => {
+        if (err) throw err;
+        else {
+            let sqlfk = `SET FOREIGN_KEY_CHECKS=1;`
+            conexion.query(sqlfk, (err, rows, fields) => {
+                if (err) throw err;
+                else {
+                    res.json({ status: 'usuario eliminado' })
                 }
-            })           
-            
+            })
+
         }
     })
 
@@ -59,31 +59,55 @@ router.delete('/:id',(req,res)=>{
 
 //modificar
 
- router.put('/:id',(req,res)=>{
-    const{id}=req.params
-    const{nombre,email}=req.body
-    let sqlfk =`SET FOREIGN_KEY_CHECKS=0;`
-    conexion.query(sqlfk,(err,rows,fields)=>{
-        if(err) throw err;
-        else{
-            
+router.put('/usuario:id', (req, res) => {
+    const { id } = req.params
+    const { nombre, email } = req.body
+    let sqlfk = `SET FOREIGN_KEY_CHECKS=0;`
+    conexion.query(sqlfk, (err, rows, fields) => {
+        if (err) throw err;
+        else {
+
         }
     })
     let sql = `UPDATE usuario SET nombre ='${nombre}',email='${email}' WHERE id_usuario ='${id}'`
-    conexion.query(sql,(err,rows,fields)=>{
-        if(err) throw err;
-        else{
-            let sqlfk =`SET FOREIGN_KEY_CHECKS=1;`
-            conexion.query(sqlfk,(err,rows,fields)=>{
-                if(err) throw err;
-                else{
-                    res.json({status:'usuario actualizado'})
+    conexion.query(sql, (err, rows, fields) => {
+        if (err) throw err;
+        else {
+            let sqlfk = `SET FOREIGN_KEY_CHECKS=1;`
+            conexion.query(sqlfk, (err, rows, fields) => {
+                if (err) throw err;
+                else {
+                    res.json({ status: 'usuario actualizado' })
                 }
             })
-            
+
         }
     })
 })
- 
+
+router.get('/comunas',(req,res)=>{
+    
+    let sql = 'select * from comuna'
+    conexion.query(sql,(err,rows,fields)=>{
+        if(err) throw err;
+        else{
+            res.json(rows)
+            console.log("res: ", res);
+        }
+    })
+})
+
+router.get('/regiones',(req,res)=>{
+    
+    let sql = 'select * from region'
+    conexion.query(sql,(err,rows,fields)=>{
+        if(err) throw err;
+        else{
+            res.json(rows)
+            console.log("res: ", res);
+        }
+    })
+})
+
 
 module.exports = router;
