4ddd0ca12ee68763f39894f831c97f083ab233c9
francojof <francojof2@gmail.com>
2022-06-24 14:20:08 -0400

    asd

--- index.js
+++ index.js
@@ -1,29 +1,13 @@
-require('dotenv').config();
-var cors = require('cors')
-require('dotenv').config();
+
+var cors = require('cors');
 const express = require('express');
 const router = require('./rutas')
 const port = (+process.env.PORT || 3000);
-
-
 const app = express();
 
 //datos a agregar
 app.use(cors())
 app.use(express.json());
 //config
-
 app.set('port',port)
-
 app.use('/api', router)
-
-
-app.listen(app.get('port'),(error)=>{
-    if(error){
-        console.log('algo malo paso'+error);
-    }
-    else{
-        console.log('Conectado en: '+port)
-    }
-
-})
