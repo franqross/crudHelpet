cf48ddbce21a4d57a07074727c5ff6feb691c808
francojof <francojof2@gmail.com>
2022-06-24 14:22:43 -0400

    asdsda

---
+++ index.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+var cors = require('cors');
+const express = require('express');
+const router = require('./rutas')
+const port = (+process.env.PORT || 3000);
+
+
+const app = express();
+
+//datos a agregar
+app.use(cors())
+app.use(express.json());
+//config
+app.set('port',port)
+app.use('/api', router)
