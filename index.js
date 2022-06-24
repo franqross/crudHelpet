18c0500379b0a06af16dd3ca927e2dd4d1d2c0db
Francisco Iturriaga <francisco382@gmail.com>
2022-06-24 15:07:07 -0400

    del dotenv

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config({ override: true })
 var cors = require('cors');
 const express = require('express');
 const router = require('./rutas');
-const port = (+process.env.PORT || 3000);
+const port = (process.env.PORT || 3000);
 
 const app = express();
 
