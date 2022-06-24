1bc14134b6606b879a8b3a57e7f1833fd15f93bd
Francisco Iturriaga <francisco382@gmail.com>
2022-06-24 13:34:25 -0400

    de nuevo

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
+require('dotenv').config();
 const mysql = require("mysql2");
 const fs = require("fs");
-require('dotenv').config();
 
 const db = mysql.createConnection({
     host: process.env.HOST,
